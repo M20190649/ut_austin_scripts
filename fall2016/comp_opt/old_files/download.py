6ca5c9ed49f35cd3057052864a8ce82acd79c570
Paul <pjruess@gmail.com>
2016-10-29 22:50:40 -0500

    updates

--- fall2016/comp_opt/old_files/download.py
+++ fall2016/comp_opt/old_files/download.py
@@ -1,3 +1,3 @@
 import webdl
 
-webdl.webdl("http://neddimitrov.org/teaching/201604CO",[".ipynb"],"/home/paul/mystuff/school/fall2016/comp_opt/old_files/")
+webdl.webdl("http://neddimitrov.org/teaching/201604CO",[".avi"],"/home/paul/school/fall2016/comp_opt/old_files/")
